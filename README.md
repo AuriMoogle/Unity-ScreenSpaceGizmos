d6a264479f8aeaf054b408f1cb5ba94abe3e7869
Anja Haumann <anja.haumann@nementic.games>
2022-02-25 21:38:21 +0100

    Add content

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Unity-ScreenSpaceGizmos
 A small utility that let you draw gizmos in 'screen space'.
+
+Unity’s default gizmos are essential for visual debugging but can only be drawn in world space. This restriction can be rather annoying if you try to debug custom UI or viewport-based camera movement. 
+
+This script solves the problem by drawing gizmos at the camera.NearClipPlane. It works with all camera projection types (perspective, orthographic) and canvas modes except world-space. In world-space just use the default Unity gizmos.
+
+Explanation and more content at my blog: http://anja-haumann.de/unity-screen-space-gizmos/
---
+++ ScreenGizmos.cs
@@ -0,0 +1,57 @@
+﻿// Author: Anja Haumann 2022 - MIT License
+// Explanation and more content at my blog: http://anja-haumann.de/unity-screen-space-gizmos/
+
+using UnityEngine;
+
+public static class ScreenGizmos
+{
+	private const float offset = 0.001f;
+
+	/// <summary>
+	/// Draws a line in screen space between the 
+	/// <paramref name="startPixelPos"/> and the 
+	/// <paramref name="endPixelPos"/>. 
+	/// </summary>
+	public static void DrawLine(
+		Canvas canvas, 
+		Camera camera, 
+		Vector3 startPixelPos, 
+		Vector3 endPixelPos)
+	{
+		if (camera == null || canvas == null)
+			return;
+
+		Vector3 startWorld = PixelToCameraClipPlane(
+			camera, 
+			canvas, 
+			startPixelPos);
+
+		Vector3 endWorld = PixelToCameraClipPlane(
+			camera, 
+			canvas, 
+			endPixelPos);
+
+		Gizmos.DrawLine(startWorld, endWorld);
+	}
+
+	/// <summary>
+	/// Converts the <paramref name="screenPos"/> to world space 
+	/// near the <paramref name="camera"/> near clip plane. The 
+	/// z component of the <paramref name="screenPos"/> 
+	/// will be overriden.
+	/// </summary>
+	private static Vector3 PixelToCameraClipPlane(
+		Camera camera, 
+		Canvas canvas,
+		Vector3 screenPos)
+	{
+		// The canvas scale factor affects the
+		// screen position of all UI elements.
+		screenPos *= canvas.scaleFactor;
+
+		// The z-position defines the distance to the camera
+		// when using Camera.ScreenToWorldPoint.
+		screenPos.z = camera.nearClipPlane + offset;
+		return camera.ScreenToWorldPoint(screenPos);
+	}
+}
